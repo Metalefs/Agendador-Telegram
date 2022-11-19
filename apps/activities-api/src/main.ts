162c3f16aa8f373d1f2cc24fdf29832e6e07907f
Jackson Ramalho <jackson.pires.rm@gmail.com>
2022-11-18 22:00:42 -0300

    deploy: fixing dependencies

--- apps/activities-api/src/main.ts
+++ apps/activities-api/src/main.ts
@@ -3,27 +3,17 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
-
 import { AppModule } from './app/app.module';
 
+
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+  const port = process.env.PORT || 3333; // <- Change "port" to "PORT"
   await app.listen(port, '0.0.0.0');
-
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
 
 bootstrap();
